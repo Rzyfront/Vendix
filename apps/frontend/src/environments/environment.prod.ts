232cad44d292f6ce65d1b09aeb27ca0343a61ce0
rzyfront <rzyfront@gmail.com>
2025-11-09 00:24:13 -0500

    fix(env): actualizar la URL de la API en el entorno de producción

--- apps/frontend/src/environments/environment.prod.ts
+++ apps/frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://api.vendix.online/api',
+  apiUrl: 'https://2bd2zjyqme.us-east-1.awsapprunner.com/api',
   vendixDomain: 'vendix.online',
 
   // Configuración para producción
