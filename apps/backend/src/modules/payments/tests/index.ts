e8c10e8052eb00d88995cc6cb7fcfa6e3b88b25a
“balmes2baxter” <balmescamilobritovargas@gmail.com>
2025-11-17 02:32:26 -0500

    fix: move test files to correct location and update tsconfig.build.json
    - Move payment test files from tests/ folder to module root
    - Add tests folder to tsconfig.build.json exclude list
    - This fixes TypeScript compilation errors during Docker build

--- apps/backend/src/modules/payments/tests/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './payments.controller.spec';
-export * from './payments.service.spec';
-export * from './payment-gateway.service.spec';
-export * from './payment-validator.service.spec';
-export * from './webhook-handler.service.spec';
-export * from './cash.processor.spec';
